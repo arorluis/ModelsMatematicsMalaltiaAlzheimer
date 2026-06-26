4cf7a2af181c01f129210d42f808d651a9c28414
arorluis <arorluis@gmail.com>
2026-06-26 11:34:50 +0200

    Add files via upload

--- nou_escombratge_lambda3.m
+++ nou_escombratge_lambda3.m
@@ -47,14 +47,9 @@ lambda_2=0.025;
 % escombratge en lambda3 incloent el equilibri
 lambda_3_eq=(pi_0*(alpha_0*lambda_2*sigma_1*xi_1 + ...
     lambda_1*sigma_2*xi_2))/(alpha_0*sigma_2*xi_2);
-
 v1=linspace(0, lambda_3_eq, 5);
 v2=linspace(lambda_3_eq, 2*lambda_3_eq, 5);
-
 lambda_3_0=[v1, v2(2:end)];
-
-t_terapia=1e6;
-
 T_vals=[250 1e3];
 Ti=0;
 
@@ -69,47 +64,34 @@ T=T_vals(it);
 tspan=[Ti T];
 
 for ilambda=1:length(lambda_3_0)
-
     lambda_3=lambda_3_0(ilambda);
-
     f=@(t,Z) model7(t,Z,omega_1,pi_0,...
         lambda_1,lambda_2,lambda_3,beta_0,...
         xi_1,xi_2,sigma_1,sigma_2,gamma_1,gamma_2,tau_0,...
         delta_1,delta_2,alpha_0,rho_1,rho_2,rho_3,nu_0);
-
+  
     [t{it,ilambda}, Z{it,ilambda}]=ode45(f,tspan,Z_0);
-
 end
 end
 %% GRÀFIQUES
 for it=1:length(T_vals)
 T=T_vals(it);
 for ivar=1:7
-
     figure
     hold on
     grid on
-
     for ilambda=1:length(lambda_3_0)
-
-        plot(t{it,ilambda}, ...
-             Z{it,ilambda}(:,ivar), ...
-             'LineWidth',1.5,...
+        plot(t{it,ilambda}, Z{it,ilambda}(:,ivar),'LineWidth',1.5,...
              'DisplayName',sprintf('$\\lambda_3/\\bar{\\lambda}_3=%.2f$',...
              lambda_3_0(ilambda)/lambda_3_eq));
-
     end
-
     xlabel('Temps (anys)')
     ylabel('Biomarcadors')
     xlim([Ti T])
     ylim([-0.2 1.1])
-
     title(sprintf('Canvi de $%s(t)$ amb $\\lambda_3$', ...
         nom_vars{ivar}), ...
         'Interpreter','latex')
-
     legend('show','Interpreter','latex','Location','best')
-
 end
 end
